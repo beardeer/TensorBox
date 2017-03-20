36547baded15c8946af96b3379028dba1b202a88
Xiaolu Xiong <beardeer@gmail.com>
2017-03-20 13:20:23 -0400

    2to3 for tensorbox

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -7,9 +7,9 @@ import itertools
 from scipy.misc import imread, imresize
 import tensorflow as tf
 
-from data_utils import (annotation_jitter, annotation_to_h5)
+from .data_utils import (annotation_jitter, annotation_to_h5)
 from utils.annolist import AnnotationLib as al
-from rect import Rect
+from .rect import Rect
 from utils import tf_concat
 
 def rescale_boxes(current_shape, anno, target_height, target_width):
